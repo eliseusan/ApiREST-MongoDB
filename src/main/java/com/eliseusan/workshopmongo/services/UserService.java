b4b41ee268395cec667e4de893abf52f62b9ce28
Eliseu <eliseusantico@gmail.com>
2022-06-07 00:16:14 -0300

    Insercao de usuario com Post

--- src/main/java/com/eliseusan/workshopmongo/services/UserService.java
+++ src/main/java/com/eliseusan/workshopmongo/services/UserService.java
@@ -7,22 +7,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.eliseusan.workshopmongo.domain.User;
+import com.eliseusan.workshopmongo.dto.UserDTO;
 import com.eliseusan.workshopmongo.repository.UserRepository;
 import com.eliseusan.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
-	@Autowired 
-	private UserRepository repo ; 
-	
-	public List<User> findAll(){
-		return repo.findAll() ;
+
+	@Autowired
+	private UserRepository repo;
+
+	public List<User> findAll() {
+		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-		}
-}
+	}
 
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail()) ; 
+	}
+}
